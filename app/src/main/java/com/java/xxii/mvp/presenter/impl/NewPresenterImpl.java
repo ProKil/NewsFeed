f2d3264a0f3bdd13b3e3cfa8a3315e814eab2a6b
吴华骏 <wuhj15@mails.tsinghua.edu.cn>
2017-09-07 12:22:54 +0800

    Upload NewsFeed project

---
+++ app/src/main/java/com/java/xxii/mvp/presenter/impl/NewPresenterImpl.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2016 咖枯 <java201313@163.com | 3772304@qq.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.java.xxii.mvp.presenter.impl;
+
+import com.java.xxii.greendao.NewsChannelTable;
+import com.java.xxii.mvp.interactor.NewsInteractor;
+import com.java.xxii.mvp.interactor.impl.NewsInteractorImpl;
+import com.java.xxii.mvp.presenter.NewsPresenter;
+import com.java.xxii.mvp.presenter.base.BasePresenterImpl;
+import com.java.xxii.mvp.view.NewsView;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+/**
+ * @author 咖枯
+ * @version 1.0 2016/6/2
+ */
+public class NewPresenterImpl extends BasePresenterImpl<NewsView, List<NewsChannelTable>>
+        implements NewsPresenter {
+
+    private NewsInteractor<List<NewsChannelTable>> mNewsInteractor;
+
+    @Inject
+    public NewPresenterImpl(NewsInteractorImpl newsInteractor) {
+        mNewsInteractor = newsInteractor;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        loadNewsChannels();
+    }
+
+    private void loadNewsChannels() {
+        mSubscription = mNewsInteractor.lodeNewsChannels(this);
+    }
+
+    @Override
+    public void success(List<NewsChannelTable> data) {
+        super.success(data);
+        mView.initViewPager(data);
+    }
+
+    @Override
+    public void onChannelDbChanged() {
+        loadNewsChannels();
+    }
+}
